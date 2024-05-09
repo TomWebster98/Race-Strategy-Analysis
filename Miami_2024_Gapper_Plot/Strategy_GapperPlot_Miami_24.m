b0883af8f493ecf67ceef1263d98d8153552d562
Tom Webster <tomwebster98@gmail.com>
2024-05-09 14:51:55 +0100

    Corrects csv data and includes pace delta bar chart to m script.

--- Miami_2024_Gapper_Plot/Strategy_GapperPlot_Miami_24.m
+++ Miami_2024_Gapper_Plot/Strategy_GapperPlot_Miami_24.m
@@ -58,12 +58,76 @@ hold off
 
 title("Miami 2024 Gapper Plot")
 xlabel("Lap Number")
-ylabel("Delta to Leader Average Lap-time")
+ylabel("Delta to Leader Average Lap-Time")
 set ( gca, 'YDir', 'reverse' )
 grid on
-xlim([1,53])
+xlim([1,57])
 %ylim([-41, 200])
 %yticks([-40,-20,0,20,40,60,80,100,120,140,160,180,200])
 legend(["VER", "PER", "LEC", "SAI", "HAM", "RUS", "ALO", "STR", "NOR", ...
     "PIA", "OCO", "GAS", "TSU", "RIC", "ALB", "SAR", "MAG", "HUL", ...
     "BOT", "ZHO"],"Location","southwest");
+
+%% Plot Only The Top 6
+
+figure(2)
+plot(lap_number, VER_Delta_Ref,"Color","#0072BD","LineWidth",1)
+hold on
+plot(lap_number, PER_Delta_Ref,"Color","#0072BD","LineStyle","--","LineWidth",1)
+plot(lap_number, LEC_Delta_Ref,"Color","#FF0000","LineWidth",1)
+plot(lap_number, SAI_Delta_Ref,"Color","#FF0000","LineStyle","--","LineWidth",1)
+plot(lap_number, NOR_Delta_Ref,"Color","#EDB120","LineWidth",1)
+plot(lap_number, PIA_Delta_Ref,"Color","#EDB120","LineStyle","--","LineWidth",1)
+yline(0)
+hold off
+
+title("Miami 2024 Gapper Plot")
+xlabel("Lap Number")
+ylabel("Delta to Leader Average Lap-Time")
+set ( gca, 'YDir', 'reverse' )
+grid on
+xlim([1,57])
+%ylim([-41, 200])
+%yticks([-40,-20,0,20,40,60,80,100,120,140,160,180,200])
+legend(["VER", "PER", "LEC", "SAI", "NOR", "PIA"],"Location","southwest");
+
+% VSC on Lap 23
+% SC on Lap 28-32
+
+%% Calculate Pace Delta - Norris to Verstappen
+
+Nor_Grad = gradient(NOR_Delta_Ref);
+Ver_Grad = gradient(VER_Delta_Ref);
+
+Pace_Delta_NORtoVER = Nor_Grad - Ver_Grad;
+
+figure(3)
+bar(Pace_Delta_NORtoVER)
+xlabel("Lap Number")
+ylabel("Pace Delta to Previous Lap")
+title("Pace Delta between Norris and Verstappen")
+grid on
+xlim([32,57])
+hold on
+yline(-0.328,"r--",LineWidth=1)
+yline(-0.655,LineStyle="--",Color="#7E2F8E",LineWidth=1)
+hold off
+
+%% Calculate Pace Delta - Norris to Verstappen
+
+Ver_Times = Miami_2024_Gapper_RefTime{1:57,"MV_Laptime_s"};
+Nor_Times = Miami_2024_Gapper_RefTime{1:57,"LN_Laptime_s"};
+
+Laptime_Delta_NOR2VER = Nor_Times - Ver_Times;
+
+figure(4)
+bar(Laptime_Delta_NOR2VER)
+xlabel("Lap Number")
+ylabel("Lap-Time Delta")
+title("Pace Delta Norris to Verstappen")
+grid on
+xlim([33,57])
+hold on
+yline(-0.328,"r--",LineWidth=1)
+yline(-0.655,LineStyle="--",Color="#7E2F8E",LineWidth=1)
+hold off
